9517daa24c916439bc2e1663d9c614e3107a0d7e
Kentaro Kawano <kentaro.kawano@gmail.com>
2022-04-10 00:08:15 +0900

    arrays

--- src/main.zig
+++ src/main.zig
@@ -31,6 +31,36 @@ pub fn assignment() void {
     //         ^
 }
 
+pub fn arrays() void {
+    // a の type は配列リテラルから推論される
+    const a = [5]u8{ 'h', 'e', 'l', 'l', 'o' };
+
+    // length は配列リテラルから推論される
+    const b = [_]u8{ 'w', 'o', 'r', 'l', 'd' };
+
+    std.log.info("{} {}", .{ a.len, b.len });
+
+    // 変数の type を記述していても配列リテラルのtypeは指定しないと行けないっぽい
+    var c: [3]i32 = [_]i32{ 1, 2, 3 };
+    // こう書くとコンパイルエラー
+    //var c: [3]i32 = { 1, 2, 3 };
+    // これもコンパイルエラー
+    //var c: [_]i32 = [_]i32{ 1, 2, 3 };
+
+    c[1] = 10;
+
+    // 要素へのアクセスは 0 オリジン
+    std.log.info("{} {} {}", .{ c[0], c[1], c[2] });
+
+    // 要素数が異なる代入はできない
+    // var d: [4]i32 = c;
+    // std.log.info("{}", .{d.len});
+    // ./src/main.zig:53:21: error: expected type '[4]i32', found '[3]i32'
+    //     var d: [4]i32 = c;
+    //                     ^
+}
+
 pub fn main() anyerror!void {
-    assignment();
+    //assignment();
+    arrays();
 }
