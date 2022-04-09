3c42bb437fef52fd2ad5e4ca1a4c91eb4d2c197c
Kentaro Kawano <kentaro.kawano@gmail.com>
2022-04-09 23:24:20 +0900

    assignment

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,24 @@
 const std = @import("std");
 
 pub fn assignment() void {
-    const constant: i32 = 5;
-    var variable: u32 = 5000;
+    const constant: i32 = 5;    // 定数
+    var variable: u32 = 5000;   // 変数
 
     std.log.info("constant {}, variable {}", .{constant, variable});
 
+    // 変数なので再代入可能
     variable = 10000;
     std.log.info("variable can be changed {}", .{variable});
 
+    // @as(type, value) で代入することもできる
     const inferred_constant = @as(i32, 5);
     var inferred_variable = @as(u32, 5000);
 
     std.log.info("inferred, constant {}, variable {}", .{inferred_constant, inferred_variable});
 
+    // typeを指定している場合には `undefined` を代入することが可能
+    var  a: i32 = undefined;
+    std.log.info("a {}", .{a}); // 何かしらの値にはなるっぽい
 }
 
 pub fn main() anyerror!void {
